81a8d40b95b65040cc9ec41e6d7b4928ce9fad26
Dean <deanleightan@gmail.com>
2026-03-09 22:20:05 +0800

    Initial commit

---
+++ imagedetection.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+
+import pathlib
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+x = tf.constant(4)
+print(x)
